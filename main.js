4c57291792270d1076de4acf3bcafbd84c930b9b
Haris Razis <haris@razis.com>
2021-11-18 23:24:51 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const pdfjsLib = require('pdfjs-dist');
+const pdfPath = './ubuntu.pdf';
+
+// Setting worker path to worker bundle.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../build/webpack/pdf.worker.bundle.js';
+
+// Loading a document.
+const loadingTask = pdfjsLib.getDocument(pdfPath);
+
+loadingTask.promise
+    .then(function (pdfDocument) {
+        // Request a first page
+        return pdfDocument.getPage(1).then(function (pdfPage) {
+            // Display page on the existing canvas with 100% scale.
+            const viewport = pdfPage.getViewport({scale: 1.0});
+            const canvas = document.getElementById('theCanvas');
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            const ctx = canvas.getContext('2d');
+            const renderTask = pdfPage.render({
+                canvasContext: ctx,
+                viewport,
+            });
+            return renderTask.promise;
+        });
+    })
+    .catch(function (reason) {
+        console.error('Error: ' + reason);
+    });
